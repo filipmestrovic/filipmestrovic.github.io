a3e3602bde395af4c4199da4660d351257c65433
Filip Meštrović <filipmestrovic@gmail.com>
2020-03-11 01:26:26 +0100

    New site

--- js/copy.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(email) {
-  const el = document.createElement('textarea');
-  el.value = email;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  document.getElementById('myTooltip').innerHTML = 'Copied to clipboard'
-}
---
+++ styles-23f42fbc2696d9c30034.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"+F2a":function(n,o,w){},qHiR:function(n,o,w){}}]);
+//# sourceMappingURL=styles-23f42fbc2696d9c30034.js.map
