1099e46d0b3ecf2619ef5d15a15a8379ddaa365c
fmestrovic <filip.mestrovic@hivetech.hr>
2020-07-08 02:19:27 +0200

    New post

---
+++ component---src-pages-about-js-e6cdc386c7ff7d8a78ae.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(t,e,a){"use strict";a.r(e),a.d(e,"pageQuery",(function(){return c}));var n=a("q1tI"),i=a.n(n),r=a("Bl7J"),o=a("vrFN");a("9eSz");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return i.a.createElement(r.a,{location:a,title:n},i.a.createElement(o.a,{title:"About me"}))};var c="1138990556"}}]);
+//# sourceMappingURL=component---src-pages-about-js-e6cdc386c7ff7d8a78ae.js.map
